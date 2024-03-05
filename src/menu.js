828cb72903ad029b0a411fa1d6857a0a30c9123d
petethomasmusique <petethomasmusique@gmail.com>
2024-03-05 11:21:25 +0000

    feature/native-menus: About dialog box

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,5 @@
-const { app, Menu } = require('electron');
+const { app, Menu, dialog, nativeImage } = require('electron');
+const path = require('path');
 
 const isMac = process.platform === 'darwin'
 
@@ -7,7 +8,19 @@ const template = [
     ...(isMac ? [{
         label: app.name,
         submenu: [
-            { role: 'about' },
+            { label: 'About', 
+              click: () => {
+                const iconPath = path.join(__dirname, '/assets/icons/q1-icon.png');
+                const icon = nativeImage.createFromPath(iconPath);
+                dialog.showMessageBox({
+                    title: 'About',
+                    message: 'Q1 Synth v' + app.getVersion(),
+                    buttons: ['OK'],
+                    icon: icon
+                 });
+              }
+              
+            },
             { type: 'separator' },
             { role: 'quit' }
         ]
