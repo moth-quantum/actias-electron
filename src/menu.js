d19ac09a2fe752a0eb8aeebf86e77609b8a68906
petethomasmusique <petethomasmusique@gmail.com>
2024-03-05 11:33:14 +0000

    feature/native-menus: Presets menu structure

--- src/menu.js
+++ src/menu.js
@@ -29,6 +29,33 @@ const template = [
     {
         label: 'File',
         submenu: [
+            { 
+                label: 'Load Preset', 
+                submenu: [
+                    { 
+                        label: 'Factory', 
+                        submenu: [
+                            { label: 'FM', click: () => console.log('Preset clicked') },
+                            { label: 'Sampler', click: () => console.log('Preset clicked') },
+                            { label: 'Granulator', click: () => console.log('Preset clicked') },
+                            { label: 'Wavetable', click: () => console.log('Preset clicked') },
+                        ],
+                    },
+                    { 
+                        label: 'Cephas Teom',  
+                        submenu: [
+                            { label: 'FM Bass', click: () => console.log('Preset clicked') },
+                            { label: 'Gravy Granulator', click: () => console.log('Preset clicked') },
+                            { label: 'Ocean Dream', click: () => console.log('Preset clicked') },
+                        ],
+                    },
+                    {   
+                        label: 'User',  
+                        submenu: [],
+                    },
+                ]
+            },
+            { type: 'separator' },
             { label: 'Import Preset', click: () => console.log('import clicked') },
             { label: 'Export preset', click: () => console.log('export clicked') },
             { type: 'separator' },
