1b056e4514acde6ca85211d08162c0059da9081f
Peter Thomas <petethomasmusique@gmail.com>
2024-08-16 09:56:33 +0100

    develop: suppress forge config changes related to code signing

--- forge.config.js
+++ forge.config.js
@@ -4,13 +4,13 @@ module.exports = {
   packagerConfig: {
     asar: true,
     icon: './src/assets/icons/q1-icon',
-    osxSign: {},
-    osxNotarize: {
-      tool: 'notarytool',
-      appleId: process.env.APPLE_ID,
-      appleIdPassword: process.env.APPLE_PASSWORD,
-      teamId: process.env.APPLE_TEAM_ID
-    }
+    // osxSign: {},
+    // osxNotarize: {
+    //   tool: 'notarytool',
+    //   appleId: process.env.APPLE_ID,
+    //   appleIdPassword: process.env.APPLE_PASSWORD,
+    //   teamId: process.env.APPLE_TEAM_ID
+    // }
   },
   rebuildConfig: {},
   makers: [
